11142344f8358c23ef53cd8ec8914afb6b17842c
msceada1 <195111856+msceada1@users.noreply.github.com.>
2026-01-21 19:06:27 +0100

    creacion de la clase Entidad

---
+++ src/main/java/domain/Entidad.java
@@ -0,0 +1,36 @@
+package domain;
+
+import exceptions.EntidadException;
+
+public abstract class Entidad {
+
+    private String nombre;
+    private String descripcion;
+
+    public Entidad(String nombre, String descripcion) throws EntidadException {
+        setNombre(nombre);
+        setDescripcion(descripcion);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) throws EntidadException {
+        if (nombre == null || nombre.isBlank()) {
+            throw new EntidadException("ERROR: La entidad debe tener nombre");
+        }
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) throws EntidadException {
+        if (descripcion == null || descripcion.isBlank()) {
+            throw new EntidadException("ERROR: La entidad debe tener decripción");
+        }
+        this.descripcion = descripcion;
+    }
+}
