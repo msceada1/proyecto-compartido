1688c577c67e6984e8e0f704495814277329a846
msceada1 <195111856+msceada1@users.noreply.github.com>
2026-01-22 13:01:00 +0100

    introduciendo la lista de objetos al constructor

--- src/main/java/domain/Habitacion.java
+++ src/main/java/domain/Habitacion.java
@@ -4,13 +4,11 @@ import exceptions.EntidadException;
 
 public class Habitacion extends Entidad {
 
-    private static final int MAX_CAPACIDAD_OBJETOS_POR_HABITACION = 5;
-
     private Objeto[] listaDeObjetos;
 
-    public Habitacion(String nombre, String descripcion) throws EntidadException {
+    public Habitacion(String nombre, String descripcion, Objeto[] listaDeObjetos) throws EntidadException {
         super(nombre, descripcion);
-        this.listaDeObjetos = new Objeto[MAX_CAPACIDAD_OBJETOS_POR_HABITACION];
+        this.listaDeObjetos = listaDeObjetos;
     }
 
     public Objeto[] getListaDeObjetos() {
