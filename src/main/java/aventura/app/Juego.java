df3b2b4a19a3a978997ab42dc74ecf9c1312dd5f
msceada1 <195111856+msceada1@users.noreply.github.com.>
2026-01-22 18:22:13 +0100

    quitando codigo antiguo

--- src/main/java/aventura/app/Juego.java
+++ src/main/java/aventura/app/Juego.java
@@ -8,81 +8,11 @@ import io.MiEntradaSalida;
 
 import java.util.Arrays;
 
-/**
- * Clase principal del juego "Tu Propia Aventura".
- * Esqueleto para la Misión 1 (UD1-UD3).
- * VUESTRO TRABAJO es rellenar todos los TODO
- */
 public class Juego {
-
-    private static String descripcionJuego = "Estas en clase y el profesor te manda a hacer una fotocopia. " +
-            "Pero de repente pulsas el boton de imprimir y resultaba ser una maquina del tiempo ." +
-            " y has despertado en un almacen lleno de cajas en las epoca del lejano oeste";
-
-    // El mapa de habitaciones.
-    private static String[] habitaciones = {
-            "Estás en el almacen lleno de cajas y hay un reluciente 'revolver' dentro de una caja abierta.",  // Posición 0
-            "Has salido a la calle principal, el almacen esta a la IZQUIERDA y a la DERECHA ves un antiguo establo.", // Posición 1
-            "Has entrado al establo,y ves un 'latigo' que esta como nuevo encima de una valla y tienes la calle principal a la IZQUIERDA.", // Posición 2
-            // Borra las habitaciones y escribe las tuyas
-    };
-
-    private static String[][] objetosMapa = {
-            {"revolver", null},           // Objetos en Habitación 0
-            {null, null},           // Objetos en Habitación 1
-            {"latigo", null},      // Objetos en Habitación 2
-    };
-
-    // El inventario del jugador. Tamaño fijo.
-    private static String[] inventario = new String[5];
-
-    // Variable que guarda la posición actual del jugador
-    private static int habitacionActual = 0; // Empezamos en la primera habitación
-
-    // --- FIN DE LA DEFINICIÓN DE DATOS ---
-
 
     public static void main(String[] args) {
 
-        boolean jugando = true; //variable boolean que reconoce si el juego sigue o termina. Mientras sea true, el programa sigue
 
-        System.out.println("¡Bienvenido a 'TU PROPIA AVENTURA'!");
-        System.out.println("------------------------------------------");
-
-        System.out.println(descripcionJuego);
-
-        System.out.println(habitaciones[0]);
-
-        System.out.println("Comandos disponibles:| ayuda | mirar | inventario | ir a la derecha | ir a la izquierda | coger objeto | salir |");
-
-        while (jugando) {
-
-            /**
-             * Lee el comando que has introducido y si es distinto a los comandos devuelve el error y te manda a escribir otra vez.
-             */
-            System.out.print("\n> ");
-            String comandoIntroducido = MiEntradaSalida.leerCadena("¿Que accion quieres hacer?");
-            while (!comandoValido(comandoIntroducido, comandos())) {
-                comandoIntroducido = MiEntradaSalida.leerCadena("Has introducido un comando incorrecto, prueba de nuevo.");
-            }
-
-            switch (comandoIntroducido) {
-                case "ayuda" -> System.out.println(Arrays.toString(comandos())); //muestra los comandos del juego
-                case "mirar" ->
-                        System.out.println(habitaciones[habitacionActual]); //muestra la habitacion donde se encuentra el usuario
-                case "inventario" -> mirarInventario(inventario); //muestra el inventario del usuario
-              //  case "ir a la derecha" ->
-                       // habitacionActual = //irALaDerecha(habitacionActual); //mueve la posicion a la derecha
-                case "ir a la izquierda" ->
-                        habitacionActual = irALaIzquierda(habitacionActual); //mueve la posicion a la izquierda
-                case "coger objeto" ->
-                        inventario = cogerObjeto(); //coge el objeto de la habitacion y lo añade al inventario
-                case "salir" -> jugando = false; //cambia el valor de jugando a false y se termina el juego
-            }
-
-        }
-
-        System.out.println("¡Gracias por jugar!");
     }
 
     /**
@@ -149,107 +79,6 @@ public class Juego {
         return false;
     }
 
-    /**
-     * Metodo que informa y muestra al usuario la lista de objetos que posee en el inventario
-     *
-     * @param inventario El array de objetos
-     */
-    private static void mirarInventario(String[] inventario) {
-
-        boolean hayObjeto = false; //variable que me permite saber si el inventario esta vacio o no
-
-        for (int i = 0; i < inventario.length; i++) {
-            if (inventario[i] != null) { //si es ditinto de null, hay un objeto y se imprime por pantalla
-                System.out.println("Posees : " + inventario[i]);
-                hayObjeto = true;
-            }
-        }
-
-        if (!hayObjeto) {
-            System.out.println("El inventario está vacío");
-        }
-    }
-
-    /**
-     * Metodo que comprueba primero que en la habitacion en la que se encuentre el usuario haya un objeto. En caso
-     * verdadero, muestra los objetos que hay en la habitacion. Se pregunta al usuario si quiere coger el objeto. En
-     * caso afirmativo, el objeto sera introducido al inventario ocupando una posicion de este y borrando el objeto del
-     * mapa
-     *
-     * @return el inventario actualizado
-     */
-    private static String[] cogerObjeto() {
-
-        if (hayObjeto()) { //se comprueba que hay objeto en la habitacion
-            listaOpciones(); //se listan los objetos
-            String objeto = MiEntradaSalida.leerCadena("¿Que objeto quieres coger?");
-            for (int i = 0; i < objetosMapa[habitacionActual].length; i++) {
-                if (objeto.equalsIgnoreCase(objetosMapa[habitacionActual][i])) {
-                    if (objetosMapa[habitacionActual][i].equalsIgnoreCase(objeto)) {
-                        guardarInventario(objeto);
-                        objetosMapa[habitacionActual][i] = null; //el hueco donde estaba el objeto se deja a null
-                    }
-                }
-            }
-
-
-        } else {
-            System.out.println("No hay objeto donde te encuentras");
-        }
-
-        return inventario;
-    }
-
-    /**
-     * Metodo que recorre los objetos de la habitacion y los muestra listado por pantalla
-     */
-    private static void listaOpciones() {
-        for (int i = 0; i < objetosMapa[habitacionActual].length; i++) {
-            if (objetosMapa[habitacionActual][i] != null) { //si es distinto de null, es un objeto y se imprime
-                System.out.println((i + 1) + ". " + objetosMapa[habitacionActual][i]);
-            }
-        }
-    }
-
-    /**
-     * Metodo que se encarga de añadir un objeto al inventario comprobando que haya espacio para el objeto entrante
-     *
-     * @param objeto el de la habitacion en la que se encuentra el usuario
-     */
-    private static void guardarInventario(String objeto) {
-
-        int ocupado = 0; //variable que se encarga de gestionar los espacios que hay ocupando el inventaerio
-
-        for (int i = 0; i < inventario.length; i++) {
-            if (inventario[i] != null) ocupado++;
-        }
-        for (int i = 0; i < inventario.length; i++) {
-            if (inventario[i] == null) {
-                inventario[i] = objeto;
-                return;
-            } else if (inventario != null) {
-                if (ocupado == inventario.length) {
-                    System.out.println("No te queda espacio en el inventario.");
-                }
-            }
-        }
-    }
-
-    /**
-     * Metodo que compruba si hay un objeto en la habitacion que se encuentra el usuario
-     *
-     * @return {@code true} si hay objeto, {@code false} en caso contrario
-     */
-    private static boolean hayObjeto() {
-
-        for (int i = 0; i < objetosMapa[habitacionActual].length; i++) {
-            if (objetosMapa[habitacionActual][i] != null) { //segun en la habitacion en la estamos comprobamos que en las columnas de objetosMapa haya un objetos.
-                return true;
-            }
-        }
-
-        return false;
-    }
 
     //a partir de aquí empieza el código de la fase 2
     private void inicializarJuego() throws EntidadException {
