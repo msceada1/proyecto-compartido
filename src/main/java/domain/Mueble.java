eeeb8ea2629e65071ae987647b085d8fc935c55f
msceada1 <195111856+msceada1@users.noreply.github.com.>
2026-01-29 19:05:05 +0100

    añadiendo getters y setters a Mueble y un nuevo atributo

--- src/main/java/domain/Mueble.java
+++ src/main/java/domain/Mueble.java
@@ -4,7 +4,18 @@ import exceptions.EntidadException;
 
 public class Mueble extends Objeto {
 
-    public Mueble(String nombre, String descripcion, boolean esVisible) throws EntidadException {
+    Objeto[] listaDeObjetosContenidos;
+
+    public Mueble(String nombre, String descripcion, Objeto[] listaDeObjetosContenidos, boolean esVisible) throws EntidadException {
         super(nombre, descripcion, esVisible);
+        this.listaDeObjetosContenidos = listaDeObjetosContenidos;
+    }
+
+    public Objeto[] getListaDeObjetosContenidos() {
+        return listaDeObjetosContenidos;
+    }
+
+    public void setListaDeObjetosContenidos(Objeto[] listaDeObjetosContenidos) {
+        this.listaDeObjetosContenidos = listaDeObjetosContenidos;
     }
 }
