74390de51170ad656401e47695996b0f3e2873a0
msceada1 <195111856+msceada1@users.noreply.github.com>
2026-01-30 09:26:43 +0100

    añadiendo nuevos cambios a la clase Mueble

--- src/main/java/domain/Mueble.java
+++ src/main/java/domain/Mueble.java
@@ -4,18 +4,7 @@ import exceptions.EntidadException;
 
 public class Mueble extends Objeto {
 
-    Objeto[] listaDeObjetosContenidos;
-
-    public Mueble(String nombre, String descripcion, Objeto[] listaDeObjetosContenidos, boolean esVisible) throws EntidadException {
+    public Mueble(String nombre, String descripcion, boolean esVisible) throws EntidadException {
         super(nombre, descripcion, esVisible);
-        this.listaDeObjetosContenidos = listaDeObjetosContenidos;
-    }
-
-    public Objeto[] getListaDeObjetosContenidos() {
-        return listaDeObjetosContenidos;
-    }
-
-    public void setListaDeObjetosContenidos(Objeto[] listaDeObjetosContenidos) {
-        this.listaDeObjetosContenidos = listaDeObjetosContenidos;
     }
 }
