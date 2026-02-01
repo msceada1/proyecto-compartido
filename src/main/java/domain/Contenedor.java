ba4373dfb883e76dbd629393e4d17f2c22b539cc
msceada1 <195111856+msceada1@users.noreply.github.com.>
2026-02-01 13:29:26 +0100

    añadiendo el metodo estaAbierto

--- src/main/java/domain/Contenedor.java
+++ src/main/java/domain/Contenedor.java
@@ -2,16 +2,21 @@ package domain;
 
 import exceptions.EntidadException;
 
-public class Contenedor extends Objeto {
+public class Contenedor extends Objeto implements Abrible {
 
     private String codigo;
-
     private Objeto[] listaDeObjetosContenidos;
+    private boolean estaAbierto;
 
     public Contenedor(String nombre, String descripcion, String codigo, Objeto[] listaDeObjetosContenidos, boolean esVisible) throws EntidadException {
         super(nombre, descripcion, esVisible);
         this.codigo = codigo;
         this.listaDeObjetosContenidos = listaDeObjetosContenidos;
+        this.estaAbierto = false;
+    }
+
+    public boolean estaAbierto() {
+        return estaAbierto;
     }
 
     public String getCodigo() {
@@ -28,5 +33,23 @@ public class Contenedor extends Objeto {
 
     public void setListaDeObjetosContenidos(Objeto[] listaDeObjetosContenidos) {
         this.listaDeObjetosContenidos = listaDeObjetosContenidos;
+    }
+
+    public void setEstaAbierto(boolean estaAbierto) {
+        this.estaAbierto = estaAbierto;
+    }
+
+    @Override
+    public RespuestaAccion abrir(Llave llave) {
+        if (estaAbierto) {
+            return new RespuestaAccion(false, "El " + getNombre() + " ya estaba abierto");
+        }
+
+        if (llave.getCodigoDeSeguridad().equalsIgnoreCase(this.codigo)) {
+            setEstaAbierto(true);
+            return new RespuestaAccion(true, "El " + getNombre() + " ha sido abierto");
+        }
+
+        return new RespuestaAccion(false, "No puedes abrir el " + getNombre() + " algo falla");
     }
 }
