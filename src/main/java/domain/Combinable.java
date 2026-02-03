0424fcf565d320dfbf403b543d8ed4cae017519f
Saquito-Coder <isaacsolis31102007@gmail.com>
2026-02-03 12:45:45 +0100

    Creando la interfaz Combinable

--- src/main/java/domain/Combinable.java
+++ src/main/java/domain/Combinable.java
@@ -1,6 +1,8 @@
 package domain;
 
+import exceptions.EntidadException;
+
 public interface Combinable {
 
-    Objeto combinar();
+    Objeto combinar(Objeto objetoACombinar) throws EntidadException;
 }
