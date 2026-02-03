92e9bfbbb188be5674215b35470764405b57b382
Saquito-Coder <isaacsolis31102007@gmail.com>
2026-02-03 12:46:08 +0100

    Modificando la clase Palo

--- src/main/java/domain/Palo.java
+++ src/main/java/domain/Palo.java
@@ -2,9 +2,17 @@ package domain;
 
 import exceptions.EntidadException;
 
-public class Palo extends Objeto {
+public class Palo extends Objeto implements Combinable {
 
     public Palo(String nombre, String descripcion, boolean esVisible) throws EntidadException {
         super(nombre, descripcion, esVisible);
+    }
+
+    @Override
+    public Objeto combinar(Objeto objetoACombinar) throws EntidadException {
+        if (!(objetoACombinar instanceof Combinable)){
+            throw new EntidadException("No se pueden combinar los objetos");
+        }
+        return new Guadania("Guadania", "Resultado de haber combinando una hoz y un palo",true);
     }
 }
